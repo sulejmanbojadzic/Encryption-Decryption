bc0746fa13f7c8dd61f21e5491c264ce7581b102
Sulejman Bojadzic <109358821+sulejmanbojadzic@users.noreply.github.com>
2023-02-20 21:27:32 +0100

    Update encryption&decryptionextended.py

--- encryption&decryptionextended.py
+++ encryption&decryptionextended.py
@@ -1,7 +1,7 @@
 import random
 letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',"0","1","2","3","4","5","6","7","8","9"," "]
 enclnum=19
-EorD=input("Do you want to encrypt or dectrypt message?\nE is for encrypt and D for Decrypt : ").upper()
+EorD=input("Do you want to encrypt or dectrypt message?\nE is for encrypt and D for decrypt : ").upper()
 def Encryption(message):
         codelist=[]
         for i in message:
